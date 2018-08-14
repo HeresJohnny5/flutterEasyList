c332756aa56932561f3bf32c7b7a2e15cf210d11
John Erickson <ericksonjohn08@hotmail.com>
2018-08-14 19:14:55 -0400

    Add initState lifecycle hook to _ProductManagerState.

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
   @override
     State<StatefulWidget> createState() {
       return _ProductManagerState();
@@ -11,7 +15,13 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+    void initState() {
+      super.initState();
+      _products.add(widget.startingProduct);
+    }
 
   @override
     Widget build(BuildContext context) {
