5fa4b2706324fa1c9a8d75825d2ddfb043ed1603
John Erickson <ericksonjohn08@hotmail.com>
2018-08-20 12:39:05 -0400

    Add static image onto the ProductPage Widget.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,16 +8,25 @@ class ProductPage extends StatelessWidget {
           title: Text('Product Detail'),
         ),
         body: Column(
-          children: <Widget>[
-            Text('Details'),
-            RaisedButton(
-              child: Text('Back'),
-              onPressed: () {
-                return Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Details'),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text('Back'),
+                  color: Theme.of(context).accentColor,
+                  onPressed: () {
+                    return Navigator.pop(context);
+                  },
+                )
+              ),
+            ],
+          ),
       );
     }
 }
