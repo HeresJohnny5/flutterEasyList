56e092fbe94717c4efcebd20acf0ff13cd0a2734
John Erickson <ericksonjohn08@hotmail.com>
2018-08-27 13:23:59 -0400

    Delete product_manager file. Create products directory and products file.

--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// LOCAL IMPORTS
+import './price_tag.dart';
+
 class Products extends StatelessWidget {
   final List<Map<String, dynamic>> products;
 
@@ -27,19 +30,7 @@ class Products extends StatelessWidget {
                       SizedBox(
                         width: 8.0,
                       ),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).accentColor,
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        child: Text(
-                          '\$${products[index]['price'].toString()}',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                      PriceTag(products[index]['price'].toString()),
                     ],
                   )
                 ),
